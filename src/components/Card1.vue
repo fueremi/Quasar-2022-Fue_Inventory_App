6b0739c4549ec47f414d3c925b2966a4fc493bcb
Rian Oktio Mersa Putra <rianoktiomersaputra4@gmail.com>
2022-03-01 00:16:46 +0700

    fix(component): split element which can components

---
+++ src/components/Card1.vue
@@ -0,0 +1,41 @@
+<template>
+  <div
+    class="row flex-center bg-white q-px-md q-py-lg q-mt-sm"
+    style="
+      border-radius: 10px;
+      box-shadow: rgba(0, 0, 0, 0.25) 0px 14px 28px,
+        rgba(0, 0, 0, 0.22) 0px 10px 10px;
+    "
+  >
+    <div class="col-3">
+      <q-icon
+        :name="icon"
+        style="
+          font-size: 40px;
+          border-radius: 100%;
+          padding: 10px;
+          color: white;
+        "
+        :style="[`background-color: ${color}`]"
+      />
+    </div>
+    <div class="col-7">
+      <h1 style="font-size: 24px; line-height: 1" class="q-ma-none text-bold">
+        {{ quantity }}
+      </h1>
+      <p class="q-ma-none" style="font-size: 16px">{{ text }}</p>
+    </div>
+    <div class="col-2 column flex-center">
+      <q-icon name="navigate_next" style="font-size: 30px" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Card1",
+  props: ["icon", "text", "color", "quantity"],
+});
+</script>
---
+++ src/components/Card2.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="col-5 bg-white q-px-md q-py-lg" style="border-radius: 10px">
+    <div :style="[`border-left: 3px solid ${color}`]">
+      <p class="q-ma-none q-pl-md" style="font-size: 14px">{{ text }}</p>
+      <p class="q-ma-none q-pl-md" style="font-size: 20px; border-left">
+        {{ quantity }}
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Card2",
+  props: ["color", "quantity", "text"],
+});
+</script>
